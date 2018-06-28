e0e2293920967dbafe590a62971116d44ac6a02c
Mario <mario@die-davids.de>
2018-06-28 09:57:00 +0200

    finished M:N test

--- modules/web/src/de/diedavids/ddcdit/web/mlb/baseballstrength/BaseballStrengthBrowse.groovy
+++ modules/web/src/de/diedavids/ddcdit/web/mlb/baseballstrength/BaseballStrengthBrowse.groovy
@@ -1,6 +1,27 @@
 package de.diedavids.ddcdit.web.mlb.baseballstrength
 
 import com.haulmont.cuba.gui.components.AbstractLookup
+import com.haulmont.cuba.gui.components.Button
+import com.haulmont.cuba.gui.components.GroupTable
+import de.diedavids.cuba.dataimport.web.WithImport
+import de.diedavids.ddcdit.entity.mlb.MlbPlayer
+import de.diedavids.ddcdit.web.RemoveAllLookup
 
-class BaseballStrengthBrowse extends AbstractLookup {
+import javax.inject.Inject
+
+@WithImport(listComponent = "baseballStrengthsTable")
+class BaseballStrengthBrowse extends RemoveAllLookup {
+
+
+    @Inject
+    protected GroupTable<MlbPlayer> baseballStrengthsTable;
+
+    @Inject
+    protected Button removeAllBtn;
+
+    @Override
+    public void init(Map<String, Object> params) {
+        super.init(params);
+        initRemoveAllAction(baseballStrengthsTable, removeAllBtn);
+    }
 }
